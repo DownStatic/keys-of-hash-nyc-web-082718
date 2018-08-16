8fe1305305dbed777c1786aafe678d237a03bf0c
Christopher Stull <chris.stull@gmail.com>
2018-08-16 22:03:50 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     keys_in = []
     arguments.each do |argument|
-      if Hash.has_value?(argument)
+      if self.has_value?(argument)
         keys_in << argument
       end
     end

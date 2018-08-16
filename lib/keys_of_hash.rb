312f61f17a368d6a9ba916f91c356fd9b8ab9638
Christopher Stull <chris.stull@gmail.com>
2018-08-16 21:25:20 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,9 @@ class Hash
   def keys_of(*arguments)
     keys_in = []
     arguments.each do |argument|
-      Hash.each do |key,value|
-        if key == argument
-          keys_in << argument
-        end
+      if Hash[:argument] == nil
+      else
+        keys_in << argument
       end
     end
   end
